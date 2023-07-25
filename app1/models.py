8fe4ba648bb1e1283e52abe38ed979b89b4ea33f
Katrin Hanikel <katrin.hanikel@gmail.com>
2023-07-25 15:50:09 +0200

    Datenbank eingepflegt

--- app1/models.py
+++ app1/models.py
@@ -9,7 +9,7 @@ class Block(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-    title = models.CharField(_("Titel"), max_length=250)
+    title = models.CharField("Titel", max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User, verbose_name="Author", on_delete=models.CASCADE)
     body = models.TextField("Inhalt")
@@ -23,7 +23,7 @@ class Block(models.Model):
         verbose_name_plural = "Blöcke"
 
     def __str__(self):
-        return self.name
+        return f"{self.title}\{self.author} ({self.created})"
 
     def get_absolute_url(self):
         return reverse("Block_detail", kwargs={"pk": self.pk})
